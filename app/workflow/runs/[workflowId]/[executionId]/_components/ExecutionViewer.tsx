8ca99fe456fa9db88d1df2173e0e7edcc53bd475
jcy97 <sook3427@naver.com>
2025-04-19 21:26:45 +0900

    [feat] Execution Detail

--- app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases";
+import { GetWorkflowPhaseDetails } from "@/actions/workflows/getWorkflowPhaseDetail";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -17,11 +18,12 @@ import {
   LucideIcon,
   WorkflowIcon,
 } from "lucide-react";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>;
 
 function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
+  const [selectedPhase, setSelectedPhase] = useState<string | null>(null);
   const query = useQuery({
     queryKey: ["execution", initialData?.id],
     initialData,
@@ -29,6 +31,13 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
     refetchInterval: (q) =>
       q.state.data?.status === WorkflowExecutionStatus.RUNNING ? 1000 : false,
   });
+
+  const phaseDetails = useQuery({
+    queryKey: ["phaseDetails", selectedPhase],
+    enabled: selectedPhase !== null,
+    queryFn: () => GetWorkflowPhaseDetails(selectedPhase!),
+  });
+  const isRunning = query.data?.status === WorkflowExecutionStatus.RUNNING;
 
   const duration = DatesToDurationString(
     query.data?.completedAt,
@@ -73,7 +82,7 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
           <ExecutionLabel
             icon={CoinsIcon}
             label="Credits consumed"
-            value="TODO"
+            value={creditsConsumed}
           />
         </div>
         <Separator />
@@ -89,16 +98,24 @@ function ExecutionViewer({ initialData }: { initialData: ExecutionData }) {
             <Button
               key={phase.id}
               className="w-full justify-between"
-              variant={"ghost"}
+              variant={selectedPhase === phase.id ? "secondary" : "ghost"}
+              onClick={() => {
+                if (isRunning) return;
+                setSelectedPhase(phase.id);
+              }}
             >
               <div className="flex itesm-center gap-2">
                 <Badge variant={"outline"}>{index + 1}</Badge>
                 <p className="font-semibold">{phase.name}</p>
               </div>
+              <p className="text-xs text-muted-foreground">{phase.status}</p>
             </Button>
           ))}
         </div>
       </aside>
+      <div className="flex w-full h-full">
+        <pre>{JSON.stringify(phaseDetails.data, null, 4)}</pre>
+      </div>
     </div>
   );
 }
--- lib/helper/phases.ts
+++ lib/helper/phases.ts
@@ -2,5 +2,10 @@ import { ExecutionPhase } from "@prisma/client";
 
 //Pick: 기존 타입에서 필요한 타입만 선택
 //ExecutionPhase에서 creditsCost만 빼서 별도 타입으로 생성
-type Phase = Pick<ExecutionPhase, "creditsCost">;
-export function GetPhasesTotalCost(phases: Phase[]) {}
+type Phase = Pick<ExecutionPhase, "creditsConsumed">;
+export function GetPhasesTotalCost(phases: Phase[]) {
+  return phases.reduceRight(
+    (acc, phase) => acc + (phase.creditsConsumed || 0),
+    0
+  );
+}
